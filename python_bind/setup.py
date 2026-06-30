d8c901fb03c9ace557d16300dd17dab5fa235c07
out0 <cristianoo@gmail.com>
2026-06-30 01:50:24 -0300

    version

--- python_bind/setup.py
+++ python_bind/setup.py
@@ -5,7 +5,7 @@ with open("app/README.md", "r") as f:
 
 setup(
     name="pyfastrrt",
-    version="2.1.0",
+    version="2.1.1",
     description="Fast RRT path planner",
     package_dir={"": "app"},
     packages=find_packages(where="app"),
